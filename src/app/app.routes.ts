072b6b652a5916e0754cc7cd992281738d643de8
dorota <doris.sajdak@gmail.com>
2025-09-29 13:30:23 +0200

    changes in routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { MoodComponent } from './components/mood/mood.component';
+import { WorkoutComponent } from './components/workout/workout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MoodComponent,
+    title: 'Home page - your best mood and workout app'
+  },
+  {
+    path: 'workout',
+    component: WorkoutComponent,
+    title: 'Workout - your best mood and workout app'
+  },
+];
