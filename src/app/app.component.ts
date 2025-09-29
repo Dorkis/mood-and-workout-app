82afcfe1fe9c0f35010b64c095a72b2a99eaefd0
dorota <doris.sajdak@gmail.com>
2025-09-29 10:44:00 +0200

    cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'mood-workout-app';
+  title = 'Your best mood and workout application is waiting for your move!';
 }
